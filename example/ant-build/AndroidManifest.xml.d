29a9fb68dafd520261af59319346bf08e0c5744b
phd013 <phd013@163.com>
2014-07-10 14:41:14 +0800

    删除example

--- example/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\work\test\platforms\android\ant-build\AndroidManifest.xml : \
-E:\work\test\platforms\android\AndroidManifest.xml \
--- example/ant-build/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-E:\work\test\platforms\android\ant-build\HelloCordova-debug-unaligned.apk : \
-E:\work\test\platforms\android\ant-build\HelloCordova.ap_ \
-E:\work\test\platforms\android\ant-build\classes.dex \
-E:\work\test\platforms\android\CordovaLib\ant-build\classes.jar \
